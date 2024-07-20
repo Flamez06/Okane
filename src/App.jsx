a052de4e85b73e9ff00c932e68b2e638466c50d3
Shuvam Mitra <shanta.mitra.sm@gmail.com>
2024-07-20 12:50:56 +0530

    templated home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,139 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { MoveDownRight } from 'lucide-react';
-import viteLogo from '/vite.svg'
-import { MoveUpRight } from 'lucide-react';
-import { Gamepad2 } from 'lucide-react';
-import './App.css'
+import {
+  MoveDownRight,
+  MoveUpRight,
+  Gamepad2,
+  House,
+  BarChart2,
+  User,
+} from "lucide-react";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <div id='d1'>
-        <div id='pfpbox'>
-          <img src="src/assets/image 2.png" alt=""/>
+      <div id="d1">
+        <div id="pfpbox">
+          <img src="src/assets/image 2.png" alt="" />
           <div>
-            <h1>Hello world</h1>
+            <p1>Hello world</p1>
             <p>Arim Kaji</p>
           </div>
         </div>
-        <div id='bb'>
-        <p1>Total Balance</p1>
-        <p>$500</p>
+        <div id="bb">
+          <div id="bb-child">
+            <p1>Total Balance</p1>
+            <p2>View all</p2>
+          </div>
+          <p>$12,394</p>
         </div>
-        <div id='eb'>
-          <div id='eb1'>
-            <div className='circle'>
-            <MoveUpRight size={28} color="#01A261" strokeWidth={2.5} />
+        <div id="eb">
+          <div id="eb1">
+            <div className="circle circle-income">
+              <MoveUpRight size={28} color="#01A261" strokeWidth={2} />
             </div>
-            <div>
-              <p>Income</p>
-              <p className='bf'>$42394</p>
+            <div id="eb1-child">
+              <p1>Income</p1>
+              <p className="bf">$42,394</p>
             </div>
           </div>
-          <div id='eb2'>
-          <div className='circle'>
-          <MoveDownRight size={28} color="#FD1B2C" strokeWidth={2.5} />
-          </div>
-            <div>
-              <p>Expense</p>
-              <p className='bf'>$30000</p>
+          <div id="eb2">
+            <div className="circle circle-expenses">
+              <MoveDownRight size={28} color="#FD1B2C" strokeWidth={2} />
+            </div>
+            <div id="eb2-child">
+              <p1>Expenses</p1>
+              <p className="bf">$30,000</p>
             </div>
           </div>
         </div>
       </div>
-      <div id='d2'>
-        <h1>
-          Expense History
-        </h1>
-        <div id='graph'></div>
+      <div id="d2">
+        <h1>Expense History</h1>
+        <div id="graph"></div>
       </div>
-      <div id='d3'>
-        <h1>Goals</h1>
-        <div className='goal-cards'>
-          <div className='bluebox'><Gamepad2 color="#0666EB" strokeWidth={2} /></div>
-          <div className='gt'>
-            <h3>Station of the play</h3>
-            <p className='bt'>Saved $2,200/80%</p>
+      <div id="d3">
+        <div id="d3-header">
+          <h1>Goals</h1>
+          <p2>View all</p2>
+        </div>
+        <div className="goal-cards">
+          <div className="bluebox">
+            <Gamepad2 color="#0666EB" strokeWidth={2} />
+          </div>
+          <div className="gt">
+            <p1>Station of the play</p1>
+            <div className="gt-bar">
+              <div className="bar-under"></div>
+              <div className="bar-over"></div>
+            </div>
+            <p2 className="bt">
+              <p className="left">Saved $2,200/80%</p>
+              <p className="right">Left $1,000 / 10%</p>
+            </p2>
           </div>
         </div>
-        <div className='goal-cards'>
-          <div className='bluebox'><Gamepad2 color="#0666EB" strokeWidth={2} /></div>
-          <div className='gt'>
-            <h3>Station of the play</h3>
-            <p className='bt'>Saved $2,200/80%</p>
+        <div className="goal-cards">
+          <div className="bluebox">
+            <Gamepad2 color="#0666EB" strokeWidth={2} />
           </div>
+          <div className="gt">
+            <p1>Station of the play</p1>
+            <div className="gt-bar">
+              <div className="bar-under"></div>
+              <div className="bar-over"></div>
+            </div>
+            <p2 className="bt">
+              <p className="left">Saved $2,200/80%</p>
+              <p className="right">Left $1,000 / 10%</p>
+            </p2>
+          </div>
+        </div>
+        <div className="goal-cards">
+          <div className="bluebox">
+            <Gamepad2 color="#0666EB" strokeWidth={2} />
+          </div>
+          <div className="gt">
+            <p1>Station of the play</p1>
+            <div className="gt-bar">
+              <div className="bar-under"></div>
+              <div className="bar-over"></div>
+            </div>
+            <p2 className="bt">
+              <p className="left">Saved $2,200/80%</p>
+              <p className="right">Left $1,000 / 10%</p>
+            </p2>
+          </div>
+        </div>
+        <div className="goal-cards">
+          <div className="bluebox">
+            <Gamepad2 color="#0666EB" strokeWidth={2} />
+          </div>
+          <div className="gt">
+            <p1>Station of the play</p1>
+            <div className="gt-bar">
+              <div className="bar-under"></div>
+              <div className="bar-over"></div>
+            </div>
+            <p2 className="bt">
+              <p className="left">Saved $2,200/80%</p>
+              <p className="right">Left $1,000 / 10%</p>
+            </p2>
+          </div>
+        </div>
+      </div>
+      <div id="nav">
+        <div className="nav-item">
+          <House color="#0666EB" strokeWidth={2} />
+        </div>
+        <div className="nav-item">
+          <BarChart2 color="#919191" strokeWidth={2} />
+        </div>
+        <div className="nav-item">
+          <User color="#919191" strokeWidth={2} />
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
