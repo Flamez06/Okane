8a72547b8a7751a4f5d4b979de6c5693feb6ee08
Atendimento2005 <gogo.june2005@gmail.com>
2024-07-24 13:49:47 +0530

    added github auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,45 @@ import Hero from "./components/home/hero";
 import Expenses from "./components/home/expenses";
 import Goals from "./components/home/goals";
 import Nav from "./components/nav";
-
+import Register from "./components/register";
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
 import "./App.css";
 
-function App() {
-  return (
-    <>
-      <Hero />
-      <Expenses />
-      <Goals />
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_PROJECT_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
+);
 
-      <Nav />
-    </>
-  );
+function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return <Register />;
+  } else {
+    return (
+      <>
+        <Hero />
+        <Expenses />
+        <Goals />
+        <Nav />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/register.jsx
@@ -0,0 +1,10 @@
+export default function Register() {
+  return (
+    <div className="container">
+      <button className="github-signup-button">
+        <img src="/github.svg" alt="" />
+        Continue with github
+      </button>
+    </div>
+  );
+}
